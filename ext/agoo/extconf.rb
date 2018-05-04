1f46d9ee0af6186f45e1287f394e961cc917a362
Peter Ohler <peter@ohler.com>
2018-05-04 15:28:19 -0400

    will travis handle generic gcc

--- ext/agoo/extconf.rb
+++ ext/agoo/extconf.rb
@@ -6,7 +6,7 @@ dir_config(extension_name)
 
 $CPPFLAGS += " -DPLATFORM_LINUX" if 'x86_64-linux' == RUBY_PLATFORM
 # Travis defaults to an older version of gcc to force a newer version.
-RbConfig::MAKEFILE_CONFIG['CC'] = "gcc-7" if 'x86_64-linux' == RUBY_PLATFORM
+#RbConfig::MAKEFILE_CONFIG['CC'] = "gcc-7" if 'x86_64-linux' == RUBY_PLATFORM
 
 create_makefile(File.join(extension_name, extension_name))
 
