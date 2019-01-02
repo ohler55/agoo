15942dcdaeb3325191697f82d53068e7886e9384
Peter Ohler <peter@ohler.com>
2019-01-01 20:54:08 -0800

    Out of bounds write check added

---
+++ ext/agoo/gqljson.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2018, Peter Ohler, All rights reserved.
+
+#include "gqljson.h"
+#include "gqlvalue.h"
+
+gqlValue
+gql_json_parse(agooErr err, const char *json, size_t len) {
+
+    // TBD
+    
+    return NULL;
+}
